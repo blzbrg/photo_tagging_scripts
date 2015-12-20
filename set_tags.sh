f17747f9c4050e1bd2e794a0684a7ace249c4439
Blzbrg <randomparanoid@gmail.com>
2015-12-19 22:24:25 -0500

    view_by_tag opens feh and calls append_tag with feh action 0. Also comment fixes.

--- set_tags.sh
+++ set_tags.sh
@@ -10,6 +10,7 @@ old_tags=`exiftool -p '$Keywords' $fname`
 prompt="Set tags"
 new_tags=`zenity --entry --title "$prompt" --text "$prompt" --entry-text "$old_tags"`
 
+# only change if zenity returns without error
 if [ "$?" -eq 0 ]; then
     exiftool -overwrite_original_in_place -Keywords="$new_tags" $fname
 fi
--- view_by_tag.sh
+++ view_by_tag.sh
@@ -1,5 +1,4 @@
-# view photos from given directory with the given tag\
-# $PHOTO_VIEWER
+# view photos from given directory with the given tag
 # if one argument given, view from current directory photos with that tag
 # if two arguments given, view from first argument directory photos with tag of second argument
 
@@ -11,4 +10,11 @@ else
     target=$2
 fi
 
-exiftool -qq -ignoreMinorErrors -p '$filename:$Keywords' $dir | grep -E --ignore-case ".*:.*$target.*"  | sed -r 's/(.*):.*/\1/'
+# get genuine absolute path to folder containing myself, from http://stackoverflow.com/questions/242538/unix-shell-script-find-out-which-directory-the-script-file-resides
+script=`readlink -f "$0"`
+scriptpath=`dirname $script`
+
+# modfy path as seen by children (so that feh has modified path for when it tries to call append_tag)
+PATH="$PATH"":$scriptpath"
+
+exiftool -qq -ignoreMinorErrors -p '$filename:$Keywords' $dir | grep -E --ignore-case ".*:.*$target.*"  | sed -r 's/(.*):.*/\1/' | feh --scale-down --info "exiftool -qq -p '[\$Keywords]' %F" --action ";append_tag.sh %F"  --filelist -
